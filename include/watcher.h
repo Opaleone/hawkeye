d759cf516e9ff760bd513387223e12d3f90714b9
Leone Varando <LVARANDO@GMAIL.COM>
2025-03-14 20:38:27 -0500

    Set up header file for watcher

---
+++ include/watcher.h
@@ -0,0 +1,21 @@
+#ifndef WATCHER_H
+#define WATCHER_H
+
+#include "../include/config.h"
+#include <unordered_map>
+#include <string>
+
+class Watcher {
+  public:
+    explicit Watcher(const Config& cfg);
+    void startListening();
+    void monitorProcesses();
+    void processIncomingData(const std::string& data);
+
+  private:
+    Config config;
+    // PID -> (restart count, program name)
+    std::unordered_map<int, std::pair<int, std::string> > processes;
+};
+
+#endif
