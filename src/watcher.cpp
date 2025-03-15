5ca5c3e29df58f41be5864febb02f1cd3394b8b5
Leone Varando <LVARANDO@GMAIL.COM>
2025-03-14 21:24:57 -0500

    Created logic to process data from socket

--- src/watcher.cpp
+++ src/watcher.cpp
@@ -1,43 +1,75 @@
 #include "../include/watcher.h"
 #include "../include/utils.h"
-#include <arpa/inet.h>
 #include <iostream>
-#include <netinet/in.h>
-#include <sstream>
-#include <sys/socket.h>
-#include <sys/types.h>
 #include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sstream>
 #include <unordered_map>
 
-Watcher::Watcher(const Config &cfg) : config(cfg) {}
+Watcher::Watcher(const Config& cfg) : config(cfg) {}
 
 void Watcher::startListening() {
-  int server_fd, new_socket;
-  struct sockaddr_in address;
-  int opt = 1;
-  int addrlen = sizeof(address);
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
 
-  server_fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (server_fd == 0) {
-    Utils::logMessage("Socket creation failed.");
-    return;
-  }
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == 0) {
+        Utils::logMessage("Socket creation failed.");
+        return;
+    }
 
-  setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
-  address.sin_family = AF_INET;
-  address.sin_addr.s_addr = INADDR_ANY;
-  address.sin_port = htons(config.port);
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(config.port);
 
-  if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
-    Utils::logMessage("Bind failed.");
-    return;
-  }
+    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+        Utils::logMessage("Bind failed.");
+        return;
+    }
 
-  if (listen(server_fd, 5) < 0) {
-    Utils::logMessage("Listen failed.");
-    return;
-  }
+    if (listen(server_fd, 5) < 0) {
+        Utils::logMessage("Listen failed.");
+        return;
+    }
 
-  Utils::logMessage("Listening on port " + std::to_string(config.port));
+    Utils::logMessage("Listening on port " + std::to_string(config.port));
+
+    while (true) {
+        new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
+        if (new_socket < 0) {
+            Utils::logMessage("Accept failed.");
+            continue;
+        }
+
+        char buffer[1024] = {0};
+        read(new_socket, buffer, 1024);
+        std::string receivedData(buffer);
+
+        processIncomingData(receivedData); // Fixed call
+
+        close(new_socket);
+    }
+}
+
+void Watcher::processIncomingData(const std::string& data) {
+    std::istringstream ss(data);
+    int pid;
+    std::string programName, secret;
+
+    ss >> pid >> programName >> secret;
+
+    if (secret != config.secret_key) {
+        Utils::logMessage("Authentication failed.");
+        return;
+    }
+
+    processes[pid] = std::make_pair(0, programName);  // Fix for expected expression error
+
+    Utils::logMessage("Monitoring PID: " + std::to_string(pid) + " (" + programName + ")");
 }
